ff4bfb43434624064e392312064b2c3a5f850d5a
tanvihkas88 <tanvihkas88@gmail.com>
2024-04-25 17:57:59 +0100

    homework_week_5

---
+++ src/homework_week_5/Programme_4_RightTriangle.java
@@ -0,0 +1,22 @@
+package homework_week_5;
+
+/**
+ * 4. Write a Java Program to print below triangle
+ * <p>
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+public class Programme_4_RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
